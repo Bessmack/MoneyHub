34e7ca9b766ddfc12ec7b8e847e852474ec7be1f
HUSSEIN-IGM <HUSSEINNGUFU205@gmail.com>
2025-09-25 09:38:57 +0300

    token auth

--- Backend/auth.py
+++ Backend/auth.py
@@ -1,3 +1,9 @@
 from functools import wraps
 from flask import request, jsonify
 from models import User
+
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = None
+        
