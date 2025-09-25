67ea64a11f863cc88a829471b544fa9ec4b1ceed
HUSSEIN-IGM <HUSSEINNGUFU205@gmail.com>
2025-09-25 09:02:25 +0300

    flask app creation

--- Backend/config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite:///moneyhub.db")
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.getenv("SECRET_KEY", "dev-secret")
-    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "dev-jwt-secret")
-
-    
