d56a3f3342ffbc4d890f572951c0d76f9e2c6180
HUSSEIN-IGM <HUSSEINNGUFU205@gmail.com>
2025-09-25 09:39:43 +0300

    token extraction from authorization header

--- Backend/auth.py
+++ Backend/auth.py
@@ -6,4 +6,11 @@ def token_required(f):
     @wraps(f)
     def decorated(*args, **kwargs):
         token = None
+
+        # Get token from header
+        if 'Authorization' in request.headers:
+            auth_header = request.headers['Authorization']
+            if auth_header.startswith('Bearer '):
+                token = auth_header.split(' ')[1]
+        
         
