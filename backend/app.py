a99fa9b04a108f2431cdade3c390d1b23d949cca
Bessmack <jenwied4lif@gmail.com>
2025-09-25 12:01:36 +0300

    Add Procfile and requirements for deployment

--- backend/app.py
+++ backend/app.py
@@ -391,4 +391,5 @@ def internal_error(error):
     return jsonify({'message': 'Internal server error!'}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(host='0.0.0.0',debug=True, port=5000)
+    
